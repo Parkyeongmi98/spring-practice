69cc62884b1c815204c93e555bf276b2d7f266be
Parkyeongmi98 <yeongmi_@naver.com>
2023-02-10 16:22:42 +0900

    packge 구조 정리

--- container/src/main/java/com/douzone/container/videosystem/DVDPlayer.java
+++ container/src/main/java/com/douzone/container/videosystem/DVDPlayer.java
@@ -3,6 +3,13 @@ package com.douzone.container.videosystem;
 public class DVDPlayer {
 	private DigitalVideoDisc dvd;
 	
+	public DVDPlayer() {
+	}
+	
+	public void setDvd(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
+	
 	public DVDPlayer(DigitalVideoDisc dvd) {
 		this.dvd = dvd;
 	}
